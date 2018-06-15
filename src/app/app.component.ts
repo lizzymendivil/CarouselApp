86acb1835d535992fd0fb92c23cca28391269415
Lizzy Mendivil Bejarano <Lizzy Mendivil@jalasoft.local>
2018-06-15 11:22:39 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  data = [
+    {
+      'name': 'kristy',
+      'type': 'img',
+      'url': 'http://unsplash.it/600/400?image=940',
+      'defaultUrl': 'https://de.aorus.com/upload/Downloads/F_20170531143736CxudUM.JPG',
+      'isDownload': true
+    },
+    {
+      'name': 'kristy',
+      'type': 'img',
+      'url': 'http://unsplash.it/600/400?image=885',
+      'defaultUrl': 'https://de.aorus.com/upload/Downloads/F_20170531143736CxudUM.JPG',
+      'isDownload': true
+    },
+    {
+      'name': 'kristy',
+      'type': 'img',
+      'url': 'http://unsplash.it/600/400?image=815',
+      'defaultUrl': 'https://de.aorus.com/upload/Downloads/F_20170531143736CxudUM.JPG',
+      'isDownload': true
+    },
+    {
+      'name': 'kristy',
+      'type': 'img',
+      'url': 'http://unsplash.it/600/400?image=401',
+      'defaultUrl': 'https://de.aorus.com/upload/Downloads/F_20170531143736CxudUM.JPG',
+      'isDownload': true
+    },
+    {
+      'name': 'kristy',
+      'type': 'img',
+      'url': 'http://unsplash.it/600/400?image=623',
+      'defaultUrl': 'https://de.aorus.com/upload/Downloads/F_20170531143736CxudUM.JPG',
+      'isDownload': true
+    },
+    {
+      'name': 'kristy',
+      'type': 'img',
+      'url': 'http://unsplash.it/600/400?image=339',
+      'defaultUrl': 'https://de.aorus.com/upload/Downloads/F_20170531143736CxudUM.JPG',
+      'isDownload': true
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxHmCarouselModule } from 'ngx-hm-carousel';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxHmCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
